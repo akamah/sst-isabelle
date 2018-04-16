e504b431ffab55497a263404c3be233a65bd6ca8
Akama Hitoshi <akamahitoshi@gmail.com>
2018-04-16 15:00:10 +0900

    add convert_monoid_SST.thy

---
+++ Convert_Monoid_SST.thy
@@ -0,0 +1,17 @@
+(* Title:   Convert_Monoid_SST.thy
+   Author:  Akama Hitoshi
+*)
+
+section \<open>Convert a Monoid SST to a ordinary SST\<close>
+
+theory Convert_Monoid_SST
+  imports Main Update SST Monoid_SST
+begin
+
+subsection \<open>Definition of another strange Transducer\<close>
+
+(* TODO: We need to define decomposition of Update *)
+
+
+
+end

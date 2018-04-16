9eac0f96c7b00ed48b6084ffd2df90887fb2af25
Akama Hitoshi <akamahitoshi@gmail.com>
2018-04-16 23:19:42 +0900

    nanimo wakaran

--- Convert_Monoid_SST.thy
+++ Convert_Monoid_SST.thy
@@ -5,13 +5,26 @@
 section \<open>Convert a Monoid SST to a ordinary SST\<close>
 
 theory Convert_Monoid_SST
-  imports Main Update SST Monoid_SST
+  imports Main Update SST Monoid_SST Decompose_update
 begin
 
 subsection \<open>Definition of another strange Transducer\<close>
 
-(* TODO: We need to define decomposition of Update *)
 
+definition \<iota> :: "('x \<Rightarrow> 'y shuffle) \<Rightarrow> 'x \<Rightarrow> ('y, 'x \<times> 'y location) update" where
+  "\<iota> \<alpha> x = synthesize (\<alpha> x, (\<lambda>y'. [(x, Inl y')]), (\<lambda>y. [(x, Inr y)]))"
 
+definition \<iota>1 :: "('x \<Rightarrow> 'y shuffle) \<Rightarrow> 'x \<Rightarrow> (('y, 'x \<times> 'y location) update + 'b) list" where
+  "\<iota>1 \<alpha> x = [Inl (\<iota> \<alpha> x)]"
+
+(* TODO: define \<tau> 
+
+definition \<tau> :: "(('y, 'x \<times> 'y location) update + ('y, 'b) update) list \<Rightarrow> ('y, 'x \<times> 'y location + 'b) update" where
+  
+
+definition \<box> :: "('x \<Rightarrow> 'x shuffle) \<times> ('x, ('y, 'b) update) update \<Rightarrow> ('x \<Rightarrow> 'x shuffle)" where
+  "\<box> \<alpha> \<theta> = "
+
+*)
 
 end
--- Decompose_Update.thy
+++ Decompose_Update.thy
@@ -5,16 +5,19 @@
 section \<open>Decomposition of an Update\<close>
 
 theory Decompose_Update
-  imports Main Update
+  imports Main Update List
 begin
 
 (* an Update can be divided into two objects:
  * Shuffle (M\<^sup>1): shuffle and concatenate variables.
- * Append  (M\<^sup>2): append and prepend strings to variables.
+ * Store   (M\<^sup>2): stores strings which'll be concatenated to variables:
+ *   Append:  append
+ *   Prepend: prepend
  *)
 
 (* an detailed index of string in Append. 
- * (x, y, k) means the k-th location of occurence of variable y in the assignment to x.
+ * (x, y, k) means the position of variable y,
+    and its k-th occurence along all variables used in the assignment to x.
  *)
 type_synonym 'y index = "'y \<times> 'y \<times> nat"
 
@@ -27,6 +30,12 @@ type_synonym ('y, 'b) append = "'y index \<Rightarrow> 'b list"
 (* Prepend *)
 type_synonym ('y, 'b) prepend = "'y \<Rightarrow> 'b list"
 
+(* Store *)
+type_synonym ('y, 'b) store = "('y, 'b) append \<times> ('y, 'b) prepend"
+
+(* location of store *)
+type_synonym 'y location = "'y index + 'y"
+
 
 subsection \<open>Utility functions\<close>
 
@@ -37,11 +46,21 @@ fun first_string :: "('x + 'b) list \<Rightarrow> 'b list" where
   "first_string (Inr b#xs) = b # first_string xs"
 
 (* skip input characters until a variable specified occurs *)
-fun skip_until :: "'y \<Rightarrow> ('y + 'b) list \<Rightarrow> ('y + 'b) list" where
+fun skip_until_var :: "('y + 'b) list \<Rightarrow> ('y + 'b) list" where
+  "skip_until_var [] = []" |
+  "skip_until_var (Inl x#xs) = xs" |
+  "skip_until_var (Inr b#xs) = skip_until_var xs"
+
+(* skip input characters until a variable specified occurs *)
+fun skip_until :: "'y index \<Rightarrow> ('y index + 'b) list \<Rightarrow> ('y index + 'b) list" where
   "skip_until y' [] = []" |
-  "skip_until y' (Inl y0#xs) = 
-    (if y' = y0 then xs else skip_until y' xs)" |
+  "skip_until (x, y, k) (Inl (x0, y0, k0)#xs) = 
+    (if k = k0 then xs else skip_until (x, y, k) xs)" |
   "skip_until y' (Inr b#xs) = skip_until y' xs"
+
+fun nth_string :: "nat \<Rightarrow> ('y + 'b) list \<Rightarrow> 'b list" where
+  "nth_string 0 xs = first_string xs" |
+  poyo: "nth_string (Suc n) xs = nth_string n (skip_until_var xs)"
 
 
 fun extract_variables :: "('x + 'b) list \<Rightarrow> 'x list" where
@@ -49,17 +68,13 @@ fun extract_variables :: "('x + 'b) list \<Rightarrow> 'x list" where
   "extract_variables (Inl x#xs) = x # extract_variables xs" |
   "extract_variables (Inr a#xs) = extract_variables xs"
 
-(* increment the counter *)
-fun increment :: "('x \<Rightarrow> nat) \<Rightarrow> 'x \<Rightarrow> ('x \<Rightarrow> nat)" where
-  "increment cnt x = (\<lambda>y. if x = y then Suc (cnt x) else cnt x)"
-
-fun give_indices_rec :: "('x \<Rightarrow> nat) \<Rightarrow> 'x \<Rightarrow> ('x + 'b) list \<Rightarrow> ('x index + 'b) list" where
+fun give_indices_rec :: "nat \<Rightarrow> 'x \<Rightarrow> ('x + 'b) list \<Rightarrow> ('x index + 'b) list" where
   "give_indices_rec cnt x [] = []" |
-  "give_indices_rec cnt x (Inl y#ys) = Inl (x, y, cnt y) # give_indices_rec (increment cnt y) x ys" |
+  "give_indices_rec cnt x (Inl y#ys) = Inl (x, y, cnt) # give_indices_rec (Suc cnt) x ys" |
   "give_indices_rec cnt x (Inr b#ys) = Inr b # give_indices_rec cnt x ys"
 
 fun give_indices :: "'x \<Rightarrow> ('x + 'b) list \<Rightarrow> ('x index + 'b) list" where
-  "give_indices x ys = give_indices_rec (\<lambda>y. 0) x ys"
+  "give_indices x ys = give_indices_rec 0 x ys"
 
 subsection \<open>Resolve\<close>
 
@@ -67,13 +82,13 @@ fun resolve_shuffle :: "('y, 'b) update \<Rightarrow> 'y shuffle" where
   "resolve_shuffle \<theta> y = extract_variables (give_indices y (\<theta> y))"
 
 fun resolve_prepend :: "('y, 'b) update \<Rightarrow> ('y, 'b) prepend" where
-  "resolve_prepend \<theta> y = first_string (give_indices y (\<theta> y))"
+  "resolve_prepend \<theta> y = first_string (\<theta> y)"
 
 fun resolve_append :: "('y, 'b) update \<Rightarrow> ('y, 'b) append" where
   "resolve_append \<theta> (x, y, k) = first_string (skip_until (x, y, k) (give_indices x (\<theta> x)))"
 
-definition resolve :: "('y, 'b) update \<Rightarrow> 'y shuffle \<times> ('y, 'b) append \<times> ('y, 'b) prepend" where
-  "resolve \<theta> = (resolve_shuffle \<theta>, resolve_append \<theta>, resolve_prepend \<theta>)"
+definition resolve :: "('y, 'b) update \<Rightarrow> 'y shuffle \<times> ('y, 'b) store" where
+  "resolve \<theta> = (resolve_shuffle \<theta>, (resolve_append \<theta>, resolve_prepend \<theta>))"
 
 subsection \<open>Synthesize\<close>
 
@@ -84,18 +99,55 @@ fun synthesize_prepend :: "('y, 'b) prepend \<Rightarrow> ('y, 'y, 'b) update'" 
   "synthesize_prepend a y = map Inr (a y) @ [Inl y]"
 
 fun synthesize_append :: "('y, 'b) append \<Rightarrow> ('y index, 'y, 'b) update'" where
-  "synthesize_append a (x, y, k) = Inl y #  map Inr (a (x, y, k))"
+  "synthesize_append a (x, y, k) = Inl y # map Inr (a (x, y, k))"
 
-definition synthesize :: "'y shuffle \<times> ('y, 'b) append \<times> ('y, 'b) prepend \<Rightarrow> ('y, 'b) update" where
+definition synthesize :: "'y shuffle \<times> ('y, 'b) store \<Rightarrow> ('y, 'b) update" where
   "synthesize sap = (case sap of (s, a, p) \<Rightarrow>
      synthesize_append a \<bullet> synthesize_shuffle s \<bullet> synthesize_prepend p)"
+
+fun lookup_store :: "('y, 'b) store \<Rightarrow> 'y location \<Rightarrow> 'b list" where
+  "lookup_store (a, p) (Inl i) = a i" |
+  "lookup_store (a, p) (Inr y) = p y"
+
+
+subsection \<open>Properties of Decomposition\<close>
+
+(* an induction rule for variable and alphabet list *)
+(* [consumes n] to skip first n assumptions at induction phase *)
+lemma xw_induct [consumes 0, case_names String VarString]:
+  assumes str: "(\<And>w. P (map Inr w))"
+  and  varstr: "(\<And>x w u. P u \<Longrightarrow> P (map Inr w @ [Inl x] @ u))"
+  shows "P xs"
+proof (induct xs rule: xa_induct)
+  case Nil
+  then show ?case
+    by (metis list.map_disc_iff str)
+next
+  case (Var x xs)
+  then show ?case
+    by (metis Nil_is_map_conv append_Cons append_self_conv2 varstr)
+next
+  case (Alpha a xs)
+  then show ?case sorry
+qed
+
+theorem resolve_inverse: "synthesize (resolve m) x = m x"
+proof (induct "m x")
+  case Nil
+  then show ?case by (simp add: resolve_def synthesize_def comp_def)
+next
+  case (Cons a xa)
+  then show ?case
+    apply (simp add: resolve_def synthesize_def comp_def hat_hom_right_ignore)
+
+  sorry
 
 subsection \<open>Example\<close>
 
 fun poyo :: "(int, char) update" where
   "poyo z = (if z = 0 then [Inr (CHR ''P''), Inl 0, Inr (CHR ''Q''), Inl 0, Inr (CHR ''R'')]
-       else if z = 1 then [Inr (CHR ''A''), Inl 0, Inr (CHR ''B''), Inl 1, Inr (CHR ''C'')]
-       else [])"
+        else if z = 1 then [Inr (CHR ''A''), Inl 0, Inr (CHR ''B''), Inl 1, Inr (CHR ''C'')]
+        else [])"
 
 lemma "resolve_prepend poyo 0 = ''P''" by simp
 lemma "resolve_prepend poyo 1 = ''A''" by simp
@@ -103,10 +155,12 @@ lemma "resolve_prepend poyo 1 = ''A''" by simp
 lemma "resolve_append poyo (0, 0, 0) = ''Q''" by simp
 lemma "resolve_append poyo (0, 0, 1) = ''R''" by simp
 lemma "resolve_append poyo (1, 0, 0) = ''B''" by simp
-lemma "resolve_append poyo (1, 1, 0) = ''C''" by simp
+lemma "resolve_append poyo (1, 1, 1) = ''C''" by simp
+
 
 lemma "resolve_shuffle poyo 0 = [(0, 0, 0), (0, 0, 1)]" by simp
 
-lemma "synthesize (resolve poyo) x = poyo x" by (simp add: synthesize_def resolve_def comp_def)
+lemma "synthesize (resolve poyo) x = poyo x"
+    apply (simp add: synthesize_def resolve_def comp_def)
 
 end

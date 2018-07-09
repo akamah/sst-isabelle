b0d84f561897bd8cef96ef72c0985b792031b321
Akama Hitoshi <akamahitoshi@gmail.com>
2018-07-09 20:58:13 +0900

    MSST built of two SSTs is typable

--- Compose_SST_SST_Type.thy
+++ Compose_SST_SST_Type.thy
@@ -3,56 +3,115 @@ theory Compose_SST_SST_Type
 begin
 
 
-definition compose_\<gamma>_pred where
-  "compose_\<gamma>_pred sst2 f q2 x m1 =
-    (\<exists>m w. SST.delta_hat sst2 (q2, w) = f (q2, x) \<and>
-           SST.eta_hat sst2 (q2, w) = m \<and>
-           m1 = resolve_shuffle m)"
+definition all_shuffles where
+  "all_shuffles sst2 q2 q2' =
+    {m1. \<exists>w. SST.delta_hat sst2 (q2, w) = q2' \<and>
+           m1 = resolve_shuffle (SST.eta_hat sst2 (q2, w))}"
 
-definition compose_\<gamma> ::
+lemma all_shuffles_first:
+  "resolve_shuffle (SST.eta sst2 (q2, a))
+ \<in> all_shuffles sst2 q2 (SST.delta sst2 (q2, a))"
+  unfolding all_shuffles_def
+  apply auto
+  apply (rule exI[of _ "[a]"])
+  apply (simp_all add: comp_right_neutral)
+  done
+  
+
+lemma all_shuffles_mult:
+  "mult_shuffles (all_shuffles sst2 q0 q1) (all_shuffles sst2 q1 q2)
+ \<subseteq> all_shuffles sst2 q0 q2"
+  unfolding mult_shuffles_def all_shuffles_def
+  apply (auto simp add: delta_append[symmetric] resolve_shuffle_distrib[symmetric])
+  by (metis SST.eta_append)
+
+fun compose_\<gamma> ::
   "('q1, 'x1, 'a, 'b) SST \<Rightarrow> ('q2, 'x2, 'b, 'c) SST \<Rightarrow>
    ('q1 \<times> ('q2 \<times> 'x1 \<Rightarrow> 'q2), 'q2 \<times> 'x1, 'x2) msst_type" where
-   "compose_\<gamma> sst1 sst2 = (\<lambda>((q1, f), (q2, x)).
-      { m1. compose_\<gamma>_pred sst2 f q2 x m1 })"
+   "compose_\<gamma> sst1 sst2 ((_, f), (q2, x)) =
+      all_shuffles sst2 q2 (f (q2, x))"
 
-lemma compose_\<gamma>_simp:
-  "compose_\<gamma> sst1 sst2 ((q1, f), (q2, x)) = { m1. compose_\<gamma>_pred sst2 f q2 x m1 }"
-  unfolding compose_\<gamma>_def by simp
+lemma idS_in_all_shuffles: 
+  "idS \<in> all_shuffles sst q2 q2"
+  unfolding all_shuffles_def
+  apply (simp)
+  apply (rule exI[of _ "[]"])
+  apply (simp add: resolve_idU_idS)
+  done
+
 
 lemma compose_\<gamma>_subset:
   "type_hom (compose_\<gamma> sst1 sst2)
-     ((q1, f), Transducer.hat2 (delta2f f (SST.delta sst2)) (eta2f (SST.eta sst2))
-                 (q2, u))
-   \<subseteq> {m1. \<exists>w. SST.delta_hat sst2 (q2, w) = SST.hat1 (delta2f f (SST.delta sst2)) (q2, u) \<and>
-              m1 = resolve_shuffle (SST.eta_hat sst2 (q2, w))}"
+     ((q1, f), Transducer.hat2 (delta2f f (SST.delta sst2)) (eta2f (SST.eta sst2)) (q2, u))
+ \<subseteq> all_shuffles sst2 q2 (SST.hat1 (delta2f f (SST.delta sst2)) (q2, u))"
 proof (induct u arbitrary: q2 rule: xa_induct)
   case Nil
-  then show ?case by (simp, metis SST.hat1.simps(1) SST.hat2.simps(1) resolve_idU_idS)
+  then show ?case by (simp add: idS_in_all_shuffles)
 next
   case (Var x xs)
-  then show ?case apply (simp add: compose_\<gamma>_simp) sorry
+  then show ?case proof (simp_all)
+    have "mult_shuffles (all_shuffles sst2 q2 (f (q2, x)))
+     (type_hom (compose_\<gamma> sst1 sst2)
+       ((q1, f), Transducer.hat2 (delta2f f (SST.delta sst2)) (eta2f (SST.eta sst2)) (f (q2, x), xs)))
+    \<subseteq> mult_shuffles (all_shuffles sst2 q2 (f (q2, x)))
+     (all_shuffles sst2 (f (q2, x))
+       (SST.hat1 (delta2f f (SST.delta sst2)) (f (q2, x), xs)))"
+      apply (rule mult_shuffles_subset)
+      apply (simp_all add: Var)
+      done
+    also have "...  \<subseteq> all_shuffles sst2 q2 (SST.hat1 (delta2f f (SST.delta sst2)) (f (q2, x), xs))"
+      by (simp add: all_shuffles_mult)
+    finally show "mult_shuffles (all_shuffles sst2 q2 (f (q2, x)))
+     (type_hom (compose_\<gamma> sst1 sst2)
+       ((q1, f), Transducer.hat2 (delta2f f (SST.delta sst2)) (eta2f (SST.eta sst2)) (f (q2, x), xs)))
+    \<subseteq> all_shuffles sst2 q2 (SST.hat1 (delta2f f (SST.delta sst2)) (f (q2, x), xs))" .
+  qed
 next
   case (Alpha a xs)
-  then show ?case sorry
+  then show ?case proof (simp_all)
+    have "mult_shuffles {resolve_shuffle (SST.eta sst2 (q2, a))}
+     (type_hom (compose_\<gamma> sst1 sst2)
+       ((q1, f),
+        Transducer.hat2 (delta2f f (SST.delta sst2)) (eta2f (SST.eta sst2))
+         (SST.delta sst2 (q2, a), xs)))
+    \<subseteq> mult_shuffles {resolve_shuffle (SST.eta sst2 (q2, a))}
+       (all_shuffles sst2 (SST.delta sst2 (q2, a))
+         (SST.hat1 (delta2f f (SST.delta sst2)) (SST.delta sst2 (q2, a), xs)))"
+      by (rule mult_shuffles_subset, simp_all add: Alpha)
+    also have "... 
+    \<subseteq> mult_shuffles (all_shuffles sst2 q2 (SST.delta sst2 (q2, a)))
+       (all_shuffles sst2 (SST.delta sst2 (q2, a))
+         (SST.hat1 (delta2f f (SST.delta sst2)) (SST.delta sst2 (q2, a), xs)))"
+      by (rule mult_shuffles_subset, simp_all add: all_shuffles_first)
+    also have "...  \<subseteq> all_shuffles sst2 q2 
+                   (SST.hat1 (delta2f f (SST.delta sst2)) (SST.delta sst2 (q2, a), xs))"
+      by (simp add: all_shuffles_mult)
+    finally show "mult_shuffles {resolve_shuffle (SST.eta sst2 (q2, a))}
+     (type_hom (compose_\<gamma> sst1 sst2)
+       ((q1, f),
+        Transducer.hat2 (delta2f f (SST.delta sst2)) (eta2f (SST.eta sst2))
+         (SST.delta sst2 (q2, a), xs)))
+   \<subseteq> all_shuffles sst2 q2 
+                   (SST.hat1 (delta2f f (SST.delta sst2)) (SST.delta sst2 (q2, a), xs))" .
+ qed
 qed
 
 
-lemma "is_type (compose_SST_SST sst1 sst2) (compose_\<gamma> sst1 sst2)"
+lemma compose_typable:
+  "is_type (compose_SST_SST sst1 sst2) (compose_\<gamma> sst1 sst2)"
   unfolding is_type_def
 proof
   show "(\<forall>x. idS \<in> compose_\<gamma> sst1 sst2 (MSST.initial (compose_SST_SST sst1 sst2), x))"
     unfolding compose_SST_SST_def
-    apply (simp add: compose_\<gamma>_def compose_\<gamma>_pred_def)
+    apply (simp add: all_shuffles_def)
     apply (metis SST.hat1.simps(1) SST.hat2.simps(1) resolve_idU_idS)
     done
 next
   show "\<forall>x q a. type_hom (compose_\<gamma> sst1 sst2) (q, MSST.eta (compose_SST_SST sst1 sst2) (q, a) x)
                 \<subseteq> compose_\<gamma> sst1 sst2 (MSST.delta (compose_SST_SST sst1 sst2) (q, a), x)"
-    apply (simp add: compose_\<gamma>_simp compose_SST_SST_def compose_\<delta>_def \<Delta>_def compose_\<eta>_def H_def)
-    apply (simp add: compose_\<gamma>_pred_def)
+    apply (simp add: compose_SST_SST_def compose_\<delta>_def \<Delta>_def compose_\<eta>_def H_def)
     apply (simp add: compose_\<gamma>_subset)
     done
 qed
-
 
 end

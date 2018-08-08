dc1bfa5e09319a0fab966fb150e84dfab07dbc1e
Akama Hitoshi <akamahitoshi@gmail.com>
2018-08-08 22:18:11 +0900

    hat_homU_iota_bounded_copy

--- Composition/Convert_Monoid_SST_Type_Misc.thy
+++ Composition/Convert_Monoid_SST_Type_Misc.thy
@@ -69,6 +69,27 @@ lemma condition_of_convert_MSST_reachable_state:
   shows "\<alpha> x \<in> \<gamma> (q, x)"
   by (meson assm_is_type assm_states condition_of_convert_MSST_state)
 
+lemma hat_homU_iota_bounded_copy:
+  fixes msst :: "('q, 'x, 'y::enum, 'a, 'b) MSST"
+  fixes \<gamma> :: "('q, 'x, 'y) msst_type"
+  fixes B :: "'k::enum boundedness"
+  assumes assm_k_bounded: "boundedness B k"
+  assumes assm_is_type: "is_type msst \<gamma>"
+  assumes assm_bounded_type: "bounded_copy_type k msst \<gamma>"
+  assumes assm_reachable: "reachable (convert_MSST B msst) (q, \<alpha>)"
+  shows "bounded k (hat_homU (\<iota> B \<alpha>) (SST.eta_hat msst (q, w) x))"
+proof -
+  have "resolve_shuffle (hat_homU (\<iota> B \<alpha>) (SST.eta_hat msst (q, w) x)) \<in> type_hom \<gamma> (q, (SST.eta_hat msst (q, w) x))"
+    by (meson assm_is_type assm_reachable condition_of_convert_MSST_state iota_alpha_type_hom reachable_def)
+  also have "... \<subseteq> \<gamma> (delta_hat msst (q, w), x)" by (simp add: type_hom_hat assm_is_type)
+  finally have in_type: "resolve_shuffle (hat_homU (\<iota> B \<alpha>) (SST.eta_hat msst (q, w) x)) \<in> \<gamma> (delta_hat msst (q, w), x)" .
+  have "reachable msst (delta_hat msst (q, w))" proof (rule reachable_delta_hat)
+    show "reachable msst q" using assm_reachable unfolding convert_MSST_def reachable_def by (auto simp add: convert_\<delta>_hat)
+  qed
+  then have "bounded_shuffle k (resolve_shuffle (hat_homU (\<iota> B \<alpha>) (SST.eta_hat msst (q, w) x)))"
+    using assm_bounded_type in_type unfolding bounded_copy_type_def by auto
+  then show ?thesis by (rule resolve_bounded_inverse)
+qed
 
 
 end

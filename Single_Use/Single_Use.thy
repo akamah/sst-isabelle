ef7dc864acb9d9ee070acb5476144f073563c603
Akama Hitoshi <akamahitoshi@gmail.com>
2018-10-03 20:26:45 +0900

    count_alpha

--- Single_Use/Single_Use.thy
+++ Single_Use/Single_Use.thy
@@ -7,6 +7,9 @@ lemma sum_cong: "\<And>x. x\<in>A \<Longrightarrow> f = g \<Longrightarrow> sum 
 definition count_var :: "[('x::finite, 'y::finite, 'b) update', 'y] \<Rightarrow> nat" where
   "count_var f y0 \<equiv> \<Sum>y\<in>UNIV. count_list (f y) (Inl y0)"
 
+definition count_alpha :: "[('x::finite, 'y::finite, 'b) update', 'b] \<Rightarrow> nat" where
+  "count_alpha f b0 \<equiv> \<Sum>y\<in>UNIV. count_list (f y) (Inr b0)"
+
 lemma "count_list (f y) (Inl x) \<le> count_var f x"
 proof -
   have "(\<Sum>y\<in>UNIV. count_list (f y) (Inl x))
@@ -125,6 +128,88 @@ proof (rule allI)
     qed
   qed
 qed
+
+lemma basic_count_alpha:
+  fixes m :: "('y::finite, 'z::finite, 'b) update'"
+  fixes xs :: "('y + 'b) list"
+  shows "count_list (hat_hom m xs) (Inr a) =
+         (\<Sum>y::'y\<in>UNIV. (count_list (m y) (Inr a) * count_list xs (Inl y))) + count_list xs (Inr a)"
+proof (induct xs rule: xa_induct)
+  case Nil
+  then show ?case by simp
+next
+  case (Var x xs)
+  then show ?case proof (simp)
+    let ?f = "\<lambda>y. count_list (m y) (Inr a) * count_list xs (Inl y)"
+    let ?g = "\<lambda>y. count_list (m y) (Inr a) * (if x = y then count_list xs (Inl y) + 1 else count_list xs (Inl y))"
+    let ?h = "\<lambda>y. count_list (m y) (Inr a) * count_list xs (Inl y) + (if x = y then count_list (m y) (Inr a) else 0)"
+    let ?l = "count_list (m x) (Inr a)"
+
+    have "sum ?g UNIV = sum ?h UNIV"
+      by (rule sum.cong, auto)
+    also have "... = sum ?f UNIV + ?l"
+      by (simp add: sum.distrib)
+    finally have "sum ?g UNIV = sum ?f UNIV + ?l".
+    then show "?l + sum ?f UNIV = sum ?g UNIV" by simp
+  qed
+next
+  case (Alpha a xs)
+  then show ?case by simp
+qed
+
+
+lemma count_alpha_comp:
+  fixes m1 :: "('y::finite, 'z::finite, 'b) update'"
+  fixes m2 :: "('x::finite, 'y, 'b) update'"
+  shows "count_alpha (m1 \<bullet> m2) a
+ = (\<Sum>y::'y\<in>UNIV. count_list (m1 y) (Inr a) * count_var m2 y) + count_alpha m2 a"
+  unfolding comp_def count_alpha_def count_var_def
+proof (simp)
+  have "(\<Sum>y\<in>UNIV. count_list (m1 y) (Inr a) * (\<Sum>x\<in>UNIV. count_list (m2 x) (Inl y)))
+      = (\<Sum>y\<in>UNIV. \<Sum>x\<in>UNIV. count_list (m1 y) (Inr a) * count_list (m2 x) (Inl y))" (is "?lhs = _")
+    by (simp add: sum_distrib_left)
+  also have "... = (\<Sum>x\<in>UNIV. \<Sum>y\<in>UNIV. count_list (m1 y) (Inr a) * count_list (m2 x) (Inl y))" (is "_ = ?rhs")
+    by (rule sum.commute)
+  finally have "?lhs = ?rhs" .
+
+  then show "(\<Sum>x\<in>UNIV. count_list (hat_hom m1 (m2 x)) (Inr a)) =
+             (\<Sum>y\<in>UNIV. count_list (m1 y) (Inr a) * (\<Sum>x\<in>UNIV. count_list (m2 x) (Inl y))) +
+             (\<Sum>x\<in>UNIV. count_list (m2 x) (Inr a))"
+    by (simp add: basic_count_alpha sum.distrib)
+qed  
+
+
+lemma count_alpha_0_comp_count_alpha_n:
+  assumes "count_alpha m1 a = 0"
+  assumes "count_alpha m2 a = n"
+  shows "count_alpha (m1 \<bullet> m2) a = n"
+  using assms by (simp add: count_alpha_comp, simp add: count_alpha_def)
+
+
+lemma count_alpha_le_1_bounded_copy:
+  assumes "count_alpha m1 a \<le> 1"
+  assumes "count_alpha m2 a = n"
+  assumes "bounded k m2"
+  shows "count_alpha (m1 \<bullet> m2) a \<le> k + n"
+proof (cases "count_alpha m1 a = 0")
+  case True
+  then show ?thesis using assms by (simp add: count_alpha_comp, simp add: count_alpha_def)
+next
+  case False
+  then have "count_alpha m1 a = 1" using assms(1) by simp
+  then have 1: "(\<Sum>y\<in>UNIV. count_list (m1 y) (Inr a)) = 1" unfolding count_alpha_def by simp
+  obtain y1 where y1: "count_list (m1 y1) (Inr a) = 1 \<and> (\<Sum>y0\<in>UNIV-{y1}. count_list (m1 y0) (Inr a)) = 0"
+    using sum1[OF finite 1] by auto
+  then have y0: "\<forall>y0\<in>UNIV-{y1}. count_list (m1 y0) (Inr a) = 0" by auto
+  have "(\<Sum>y\<in>UNIV. count_list (m1 y) (Inr a) * count_var m2 y) = (\<Sum>y\<in>UNIV. if y = y1 then count_var m2 y else 0)"
+    by (rule sum.cong, auto simp add: y0 y1)
+  also have "... = count_var m2 y1" by simp
+  finally have *: "(\<Sum>y\<in>UNIV. count_list (m1 y) (Inr a) * count_var m2 y) = count_var m2 y1" .
+  show ?thesis using assms unfolding bounded_def
+    apply (simp add: count_alpha_comp, simp add: count_alpha_def)
+    using * by simp
+qed
+
 
 section \<open>Bounded-ness of SST\<close>
 
